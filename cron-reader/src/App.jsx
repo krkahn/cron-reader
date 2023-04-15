1466c44930e88a6c867a5fcdef24d295a7123831
Kris Kahn <krkahn@gmail.com>
2023-04-15 12:10:31 -0400

    modal inclusion

--- cron-reader/src/App.jsx
+++ cron-reader/src/App.jsx
@@ -3,27 +3,41 @@ import { Container, Box } from '@chakra-ui/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import TextInput from './components/TextInput'; 
+import KeywordsModal from './components/KeywordsModal';
 
 const App = () => {
   const [keywords, setKeywords] = useState('');
   const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(false);
-  const extractKeywords = (text) => {
+  const extractKeywords = async (text) => {
     setLoading(true);
     setIsOpen(true);
 
     const options = {
       method: 'POST',
       headers: {
-        'Content-Type': 'applications/JSON',
+        'Content-Type': 'application/JSON',
         Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`
       },
       body: JSON.stringify({
         model: 'text-davinci-003',
-        prompt: 'Create a cron expression from this prompt'
+        prompt: 'Create a cron expression from this prompt\n\n' + text + ''
       })
     }
+
+    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
+
+    const json = await response.json();
+    const data = json.choices[0].text.trim(); 
+    console.log(data);
+    setKeywords(data);
+    setLoading(false);
   };
+
+  const closeModal = () => {
+    setIsOpen(false); 
+  };
+
   return (
     <Box bg='green.600' color='white' height='100vh' paddingTop={130}>
       <Container maxW='3xl' centerContent>
@@ -31,6 +45,7 @@ const App = () => {
       <TextInput extractKeywords={extractKeywords}/>
       <Footer/>
       </Container>
+      <KeywordsModal keywords={keywords} loading={loading} isOpen={isOpen} closeModal={closeModal}/> 
     </Box>
   );
 };
