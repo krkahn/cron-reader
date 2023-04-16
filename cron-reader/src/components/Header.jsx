21dbe2c7b664d2ca21751b9caa6e33da66ec1564
Kris Kahn <krkahn@gmail.com>
2023-04-16 14:36:20 -0400

    font changes

--- cron-reader/src/components/Header.jsx
+++ cron-reader/src/components/Header.jsx
@@ -1,18 +1,25 @@
-import { Heading, Image, Text } from "@chakra-ui/react"
+import { Image, Heading, Text, Container } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../theme'
 import logo from '../assets/square-clock.png'
+import '@fontsource/raleway/400.css'
+import '@fontsource/open-sans/700.css'
 
 const Header = () => {
   return (
-  <>
-      <Image src = {logo} alt = 'logo' width = { 100 } marginBottom = '1rem' />
-          <Heading color ='white' marginBottom='1rem'>
-        Cron Expressor
-      </Heading>
-    <Text fontSize={25} textAlign='center'>
-     Type when you want your cron to run.
-    </Text>
-  </>
-  );
-};
+    <ChakraProvider theme={theme}>
+      <Container textAlign='center'>
+        <Image src={logo} alt='logo' width={100} marginBottom='1rem' mx='auto' />
+        <Heading color='white' marginBottom='1rem'>
+          Cron Expressor
+        </Heading>
+        <Text fontSize={25} >
+          Type when you want your cron to run.
+        </Text>
+      </Container>
+    </ChakraProvider>
+  )
+}
 
 export default Header
+
