e5a6bc3b0ff1efdc41fc00a248ed073defb714ab
Kris Kahn <krkahn@gmail.com>
2023-04-16 14:47:21 -0400

    font changes

--- cron-reader/src/theme.jsx
+++ cron-reader/src/theme.jsx
@@ -2,7 +2,7 @@ import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
   fonts: {
-    heading: `'Open Sans', sans-serif`,
+    heading: `'Raleway', sans-serif`,
     body: `'Raleway', sans-serif`,
   },
 })
